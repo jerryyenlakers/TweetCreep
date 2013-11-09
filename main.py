427c1334b7ac54e763b9f61996b33e07e9b99ec7
Hunter McElroy <huntergmcelroy@gmail.com>
2013-11-08 21:08:52 -0600

    created initial UI

---
+++ main.py
@@ -0,0 +1,13 @@
+import webapp2
+import os
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+    	path = os.path.join(os.path.dirname(__file__), 'html' ,'index.html')
+        self.response.write(template.render(path, {}))
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
